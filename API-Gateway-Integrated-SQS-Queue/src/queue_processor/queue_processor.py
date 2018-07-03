f34047fa4aca7f984c76e0e2dfcc0d12b8e0582e
Bryson Tyrrell <bryson.tyrrell@gmail.com>
2018-07-03 12:52:07 -0500

    artificial throttling of processing messages

--- API-Gateway-Integrated-SQS-Queue/src/queue_processor/queue_processor.py
+++ API-Gateway-Integrated-SQS-Queue/src/queue_processor/queue_processor.py
@@ -1,6 +1,12 @@
+import json
+import time
+
+
 def lambda_handler(event, context):
 
     for record in event['Records']:
-        print(record)
+        body = json.loads(record['body'])
+        print(f'SQS message content: {body}')
+        time.sleep(2)
 
     return {}
