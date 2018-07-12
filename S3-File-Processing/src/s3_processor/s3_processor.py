32d1136b36f34ac1bd9bc8a74899fb5ffe2f58f0
Bryson Tyrrell <bryson.tyrrell@gmail.com>
2018-07-11 23:00:49 -0400

    MacAdmins 2018 content

---
+++ S3-File-Processing/src/s3_processor/s3_processor.py
@@ -0,0 +1,45 @@
+import io
+import json
+import logging
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+import boto3
+from botocore.vendored import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+xray_recorder.configure()
+patch(['boto3', 'requests'])
+
+
+def get_file(bucket, key):
+    logger.info("Downloading '%s' from S3 Bucket '%s'", key, bucket)
+    bucket = boto3.resource('s3').Bucket(bucket)
+    f_obj = io.BytesIO()
+    try:
+        bucket.download_fileobj(Key=key, Fileobj=f_obj)
+    except:
+        logger.error('The download failed!')
+        raise
+    else:
+        logger.info('Download succeeded')
+    return json.loads(f_obj.getvalue())
+
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        object_key = record['s3']['object']['key']
+
+        data = get_file(bucket_name, object_key)
+
+        # Perform file processing...
+        requests.put(
+            'https://my/service',
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'}
+        )
+
+    return {}
---
+++ readme.md
@@ -0,0 +1,14 @@
+MacAdmins 2018 Links
+--------------------
+
+AWS SAM (Serverless Application Model)
+ https://github.com/awslabs/serverless-application-model/
+
+CloudFormation Psuedo Parameters 
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/pseudo-parameter-reference.html
+
+Lambda Event Examples
+ https://docs.aws.amazon.com/lambda/latest/dg/eventsources.html
+
+X-Ray for Python 
+https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python.html
